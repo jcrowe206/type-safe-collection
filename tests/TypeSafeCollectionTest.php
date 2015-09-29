7a30974f459f43c4fd1f7eb882e09765073bf35d
Jonathan Crowe <jcrowe206@gmail.com>
2015-09-28 19:57:57 -0700

    added a comment

--- tests/TypeSafeCollectionTest.php
+++ tests/TypeSafeCollectionTest.php
@@ -105,6 +105,10 @@ class TypeSafeCollectionTest extends TestCase {
 
 }
 
+/* ---------------------------------------------------------
+ *  Mock classes below
+ * ---------------------------------------------------------
+ */
 class CollectionTestMockNoRestrictions extends TypeSafeCollection {}
 
 
